db6ec990cdc5e41667e20da51c6259af43a9d244
surkhabmundi <surkhabm1@gmail.com>
2024-10-06 21:21:20 -0600

    Wrote binary search algorithm!

--- src/binary_search.java
+++ src/binary_search.java
@@ -1,2 +1,28 @@
 public class binary_search {
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8};
+        int target = 8;
+        int blast = binary_target(array,target);
+        System.out.println("The target is at index: " + blast);
+    }
+
+    private static int binary_target(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (array[mid] == target) {
+                return mid;
+            }
+            else if (array[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
 }

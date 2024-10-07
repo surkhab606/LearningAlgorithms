3c86e8fbb3b05d8d5cc3c855613854b5bbbe013d
surkhabmundi <surkhabm1@gmail.com>
2024-10-06 21:05:01 -0600

    Set up main method where we will run the algorithm. Setting up our method now. Using psuedocode from GPT.

--- src/reverse_string.java
+++ src/reverse_string.java
@@ -1,4 +1,13 @@
 public class reverse_string {
 
+    public static void main(String[] args) {
+        String userString = "Johnson";
+        String reversed = reverse_mode(userString);
+        System.out.println("The string reversed is: " + reversed);
+    }
 
+    private static String reverse_mode(String userString) {
+        String result = " ";
+
+    }
 }

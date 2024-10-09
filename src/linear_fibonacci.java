5f572297bfe442eee027a6f30c70dd1aeabd0ee2
surkhabmundi <surkhabm1@gmail.com>
2024-10-09 16:05:05 -0600

    Creating linear_fibonacci sequence algorithm that iterates through loops to calculate the n^th number with two base cases, 0 and 1.

---
+++ src/linear_fibonacci.java
@@ -0,0 +1,17 @@
+public class linear_fibonacci {
+
+    public static void main(String[] args) {
+        int k = 3;
+    }
+
+    private static int linear_fibo(int number) {
+        if (number == 0) {
+            return 0;
+        }
+
+        else if(number == 1) {
+            return 1;
+        }
+
+    }
+}
--- src/linear_search.java
+++ src/linear_search.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 public class linear_search {
 
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5,6,7,8};
-        int k = 7;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int k = scanner.nextInt();
         int execute = target_hunt(array, k);
+        System.out.println("The target is: " + execute);
     }
 
     private static int target_hunt(int[] array, int target) {

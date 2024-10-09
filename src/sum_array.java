31dc6ce5902452d9f5623d4ac6efca98857c99bc
surkhabmundi <surkhabm1@gmail.com>
2024-10-09 15:32:30 -0600

    Wrote sum of an array algorithm all by myself. Not the best running time, (O(n)), but not the worst running time either. Will learn hashmaps and the world will run a little faster.

--- src/sum_array.java
+++ src/sum_array.java
@@ -1,12 +1,12 @@
 public class sum_array {
 
     public static void main(String[] args) {
-        int[] array = {1,2,3,4};
-        int sum = sumArray(array);
+        int[] array = {1,2,3,4,5};
+        int sum = total_sum(array);
         System.out.println("The sum of the array is: " + sum);
     }
 
-    private static int sumArray(int[] array) {
+    private static int total_sum(int[] array) {
         int total = 0;
         for (int i = 0; i < array.length; i++) {
             total += array[i];

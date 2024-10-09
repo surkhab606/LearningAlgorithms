a879c83dc0802aa31cf891cca058c20811065e0a
surkhabmundi <surkhabm1@gmail.com>
2024-10-09 15:45:03 -0600

    Creating linear search algorithm, that goes through entire array and searches for a certain target. Should be around O(n) running time.

--- src/linear_search.java
+++ src/linear_search.java
@@ -1,2 +1,17 @@
 public class linear_search {
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8};
+        int k = 7;
+        int execute = target_hunt(array, k);
+    }
+
+    private static int target_hunt(int[] array, int target) {
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == target) {
+                return array[i];
+            }
+        }
+        return -1;
+    }
 }

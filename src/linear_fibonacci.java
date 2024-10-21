163f46659e822a5dcbb1524db2cf0f5ddcf2f844
surkhabmundi <surkhabm1@gmail.com>
2024-10-20 22:40:47 -0600

    Gonna code it solo for sure today.

--- src/linear_fibonacci.java
+++ src/linear_fibonacci.java
@@ -7,24 +7,6 @@ public class linear_fibonacci {
     }
 
     public static int linear(int number) {
-        if (number == 0) {
-            return 0;
-        }
-
-        else if (number == 1) {
-            return 1;
-        }
-
-        int a = 0;
-        int b = 1;
-
-        for(int i = 2; i <= number; i++) {
-            int next = a + b;
-            a = b;
-            b = next;
-        }
-
-        return b;
 
     }
 }

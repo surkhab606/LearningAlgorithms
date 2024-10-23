d5f77f5abf22d127ec5fb48c67864181b82d9a36
surkhabmundi <surkhabm1@gmail.com>
2024-10-22 19:49:54 -0600

    So I have the public static void down where we will be invoking the algorithm. Just trying to see if I know how to code the algo. itself so not concerned with variable user input, and just hard coded a target number.

--- src/linear_fibonacci.java
+++ src/linear_fibonacci.java
@@ -1,3 +1,12 @@
 public class linear_fibonacci {
+    public static void main(String[] args) {
+        int target = 10;
+        int result = linear(target);
+        System.out.println("The 10th number in the sequence is: " + result);
+    }
 
+
+    public static int linear(int number) {
+        return -1;
+    }
 }

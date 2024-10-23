6b9b9c73d44edac5e972069ec035ee9ce09f7d1a
surkhabmundi <surkhabm1@gmail.com>
2024-10-22 20:17:05 -0600

    Added .toLowerCase so that it looks better.

--- src/reverse_string.java
+++ src/reverse_string.java
@@ -1,13 +1,16 @@
 public class reverse_string {
 
     public static void main(String[] args) {
-        String userString = "Johnson";
+        String userString = "surkhab";
         String reversed = reverse_mode(userString);
         System.out.println("The string reversed is: " + reversed);
     }
 
     private static String reverse_mode(String userString) {
         String result = "";
-
+        for (int i = userString.length() - 1; i >= 0; i--) {
+            result = result + userString.charAt(i);
+        }
+        return result.toLowerCase();
     }
 }

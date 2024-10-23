bcf40128819e96329af9df726fd024c5d6dfedcc
surkhabmundi <surkhabm1@gmail.com>
2024-10-22 19:44:48 -0600

    Recoding all of linear_fibonacci.java

--- src/linear_fibonacci.java
+++ src/linear_fibonacci.java
@@ -1,29 +1,3 @@
 public class linear_fibonacci {
 
-    public static void main(String[] args) {
-        int target = 10;
-        int result = linear(target);
-        System.out.println("The 10th number in the sequence is " + result);
-    }
-
-    public static int linear(int number) {
-        if (number == 1) {
-            return 1;
-        }
-
-        if (number == 0) {
-            return 0;
-        }
-
-        int a = 0;
-        int b = 1;
-
-        for(int i = 2; i <= number; i++) {
-            int next = a + b;
-            a = b;
-            b = next;
-        }
-
-        return b;
-    }
 }
